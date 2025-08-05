91ab2fafac387d8572572649bd083173f7846aa1
Trchien123 <huynhtrungchienltt@gmail.com>
2025-08-05 23:43:03 +0700

    reset

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/react-minesweeper/",
   plugins: [react()],
   css: {
     postcss: {
