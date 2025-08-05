1b9cff0e4860d7c42c39010fa898fe01968e65bf
dannguyen0309 <169075433+dannguyen0309@users.noreply.github.com>
2025-08-05 23:22:39 +0700

    Deploy

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://minesweeper-ai-flame.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
